c06899d39e0672dc263dffe0b909953a9031a503
ihongjinlee <ihongjinlee@gmail.com>
2024-01-24 16:02:13 +0900

    Sanity User 스키마 team, email 속성 삭제 close #12

--- sanity-studio/schemas/user.js
+++ sanity-studio/schemas/user.js
@@ -9,27 +9,9 @@ export default {
       type: 'string',
     },
     {
-      title: 'email',
-      name: 'email',
-      type: 'string',
-    },
-    {
       title: 'name',
       name: 'name',
       type: 'string',
-    },
-    {
-      title: 'team',
-      name: 'team',
-      type: 'string',
-      options: {
-        list: [
-          {title: 'pm', value: 'pm'},
-          {title: 'marvel', value: 'marvel'},
-          {title: 'dc', value: 'dc'},
-        ],
-        layout: 'radio',
-      },
     },
     {
       title: 'image',
