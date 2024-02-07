4dedf940b9eb7a0b56788c8253efe04f341bca02
ihongjinlee <ihongjinlee@gmail.com>
2024-02-07 16:55:32 +0900

    git commit -m 유저 상세 페이지 구현하기 #17 - 컴포넌트(UserProfile)

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,9 +1,22 @@
 import { User } from '@/model/user';
+import Avatar from './Avatar';
 
 type Props = {
   user: User;
 };
 
-export default function UserProfile({ user: { user_id } }: Props) {
-  return <p>{user_id}</p>;
+export default function UserProfile({ user: { image, user_id, name } }: Props) {
+  return (
+    <section className='w-full flex flex-col md:flex-row items-center justify-center py-8 border-b border-neutral-300 dark:border-neutral-700'>
+      <Avatar image={image} size='xlarge' priority />
+      <div className='md:ml-10 text-center'>
+        <div className='flex flex-col items-center md:flex-row'>
+          <h1 className='text-2xl md:mr-8 my-2 md:mb-0 dark:text-gray-300 '>
+            {name}
+          </h1>
+        </div>
+        <p className='text-xl md:text-start dark:text-gray-500'>{user_id}</p>
+      </div>
+    </section>
+  );
 }
