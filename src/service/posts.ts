b73207d5b9a08409cecd3881296e4acb2e5c6174
ihongjinlee <ihongjinlee@gmail.com>
2024-02-07 18:14:21 +0900

    유저 상세 페이지 구현하기 #17 - 서비스(UserPost)

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,26 +1,40 @@
-import { Post } from '@/model/post';
 import { client, urlFor } from './sanity';
 import { User } from '@/model/user';
+import { Post } from '@/model/post';
+
+const simplePostProjection = `
+    ..., 
+    "id":_id,
+    members[]->{user_id, name, image}
+`;
 
 export async function getPosts() {
   return client
     .fetch(
       `
   *[_type=="post"] 
-  | order(releasedate desc){
-    ..., 
-    "id":_id,
-    members[]->{user_id, name, image}
-  }`
+  | order(releasedate desc){${simplePostProjection}}`
     )
-    .then((posts: Post[]) => {
-      return posts.map((post: Post) => ({
-        ...post,
-        photo: urlFor(post.photo, 1125),
-        members: post.members.map((user: User) => ({
-          ...user,
-          image: urlFor(user.image, 288),
-        })),
-      }));
-    });
+    .then(mapPosts);
+}
+
+export async function getPostsOf(user_id: string) {
+  return client
+    .fetch(
+      `
+    *[_type=="post" && ("${user_id}" in members[]->user_id)]
+    | order(releasedate desc){${simplePostProjection}}`
+    )
+    .then(mapPosts);
+}
+
+function mapPosts(posts: Post[]) {
+  return posts.map((post: Post) => ({
+    ...post,
+    photo: urlFor(post.photo, 1125),
+    members: post.members.map((user: User) => ({
+      ...user,
+      image: urlFor(user.image, 288),
+    })),
+  }));
 }
